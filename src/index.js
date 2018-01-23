7078dc58ea5bec8d7c060429d4360e785c51c119
Pearl Zeng <zhuzeng@umich.edu>
2018-01-23 16:23:18 -0500

    Displays sender, subject, timestamp, and snippet on page

--- src/index.js
+++ src/index.js
@@ -2,23 +2,47 @@
  * @flow
  */
 
-// example of generating HTML with js
+import store, {
+  getMessageSubject,
+  getMessageSender,
+  getMessageTimestamp,
+  getMessageSnippet,
+} from './store';
+
+function getThreadIds() {
+  let threadIds;
+  if (store.mailboxes.INBOX) {
+    threadIds = store.mailboxes.INBOX.threadIds;
+  } else {
+    threadIds = [];
+  }
+  return threadIds;
+}
+
 function renderSidebar() {
   const sidebarContents = `
     <h2 class="email-header">Inbox</h2>
-    <ul class="email-list">
-      <li>
-        <button class="email-item" type="button">
-          <div class="sender-details">
-            <p>Email sender</p>
-            <span>Timestamp</span>
-          </div>
-          <p class="email-subject">Email1 subject</p>
-          <p>Email snippet</p>
-        </button>
-      </li>
-    </ul>
-  `;
+    <ul class="email-list">${getThreadIds()
+    .map((threadId) => {
+      const message =
+          store.threads[threadId].messages[
+            store.threads[threadId].messages.length - 1
+          ];
+      return `<li>
+            <button class="email-item" type="button">
+              <div class="sender-details">
+                <p>${getMessageSender(store.messages[message.id]) || ''}</p>
+                <span>${getMessageTimestamp(store.messages[message.id]) ||
+                  ''}</span>
+              </div>
+              <p class="email-subject">
+                ${getMessageSubject(store.messages[message.id]) || ''}</p>
+              <p>${getMessageSnippet(message)}</p>
+            </button>
+          </li>`;
+    })
+    .join('')}</ul>
+    `;
 
   const container = document.querySelector('.email-list-container');
   if (container != null) container.innerHTML = sidebarContents;
